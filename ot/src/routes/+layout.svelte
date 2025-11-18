f7643f20f5480766144960530a7daa70daaf964a
Dmitry Medvedev <dmitry.medvedev@gmail.coom>
2025-11-18 18:04:46 +0100

    in production: disable pointer-events and user-select

--- ot/src/routes/+layout.svelte
+++ ot/src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { dev } from "$app/environment";
   import "../app.css";
   import favicon from "$lib/assets/favicon.svg";
 
@@ -12,10 +13,17 @@
   <meta name="description" content={siteInfo.meta.description} />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta name="theme-color" content="#000000" />
-
   {@html `<script type="application/ld+json">${JSON.stringify(JSONLD)}</script>`}
-
   <link rel="icon" href={favicon} />
+
+  {#if !dev}
+    <style>
+      :root {
+        pointer-events: none;
+        user-select: none;
+      }
+    </style>
+  {/if}
 </svelte:head>
 
 {@render children()}
